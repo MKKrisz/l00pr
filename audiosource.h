d5c34c604114743b3bc82d94c7b8603e7160dfe6
MKKrisz <magyarikrisztianbusiness@gmail.com>
2024-04-07 17:21:39 +0200

    File hierarchy changes 2 electric boogaloo

--- audiosource.h
+++
@@ -1,8 +0,0 @@
-#ifndef L00PR_AUDIOSOURCE_H
-#define L00PR_AUDIOSOURCE_H
-
-class AudioSource {
-    
-};
-
-#endif
--- generators/none.cpp
+++
@@ -1,5 +0,0 @@
-#include "none.hpp"
-
-NoGenerator::NoGenerator() {}
-float NoGenerator::getSample(float phase, float t) { return 0; }
-Generator* NoGenerator::copy() {return new NoGenerator();}
