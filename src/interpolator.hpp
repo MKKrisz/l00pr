3d0e5e3e5f328dd5cbd62627f95278033e38bba5
MKKrisz <magyarikrisztianbusiness@gmail.com>
2024-04-15 01:00:57 +0200

    Made interpolators even more generic

--- src/interpolator.hpp
+++ src/interpolator.hpp
@@ -3,17 +3,18 @@
 
 #include <cmath>
 
-template <typename T>
-T noInterpolator(T a, T b, float t) { return a; }
 
-template <typename T>
-T reverseInterpolator(T a, T b, float t) { return b; }
+template <typename T, std::floating_point F = double>
+T noInterpolator(T a, T b, F t) { return a; }
 
-template <typename T>
-T linearInterpolator(T a, T b, float t) { return (1-t)*a + t*b; }
+template <typename T, std::floating_point F = double>
+T reverseInterpolator(T a, T b, F t) { return b; }
 
-template <typename T>
-T logarithmicInterpolator(T a, T b, float t) { return pow(a, 1-t)*pow(b, t);}
+template <typename T, std::floating_point F = double>
+T linearInterpolator(T a, T b, F t) { return (1-t)*a + t*b; }
+
+template <typename T, std::floating_point F = double>
+T logarithmicInterpolator(T a, T b, F t) { return pow(a, 1-t)*pow(b, t);}
 
 
 
