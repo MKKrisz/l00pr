41a11aa38c3efc388e959d80669db8f93b11383c
MKKrisz <magyarikrisztianbusiness@gmail.com>
2024-05-10 09:03:29 +0200

    Prep for pluginization

--- main.cpp
+++ main.cpp
@@ -1,20 +1,33 @@
 #include "src/argmgr.hpp"
 #include "src/pstate.hpp"
+#include "src/generator/generator.hpp"
+#include "src/filter/filter.hpp"
+#include "src/tune/tune.hpp"
 #include <SDL2/SDL.h>
+#include <gtest/gtest.h>
 #include <ctime>
 
 
 /// <summary>
 /// Entry point of the code. Standard parameters, nothing special
 /// </summary>
-int main(int argc, const char** argv) {
+int main(int argc, char** argv) {
+    srand(time(0));
+    Generator::Init();
+    Filter::Init();
+    Tune::Init();
+
+#ifdef CPORTA
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+#endif
+
     // Initialize SDL and random
     SDL_Init(SDL_INIT_AUDIO);
-    srand(time(0));
 
-    ArgumentManager argmgr{args};
+    ArgumentManager argmgr{pstate.args};
     argmgr.Parse(argc, argv);
-    ifs = argmgr.getUnparsed();
+    pstate.ifs = argmgr.getUnparsed();
 
     //Process and run arguments
     Run();
