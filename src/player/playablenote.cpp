a94e21781c7c802aed0f88b613a83bad856fec8f
MKKrisz <magyarikrisztianbusiness@gmail.com>
2025-07-24 14:50:59 +0200

    Created base class that can be inherited from if the plugin developer wants a note that is affectd by note modifying filters

--- src/player/playablenote.cpp
+++ src/player/playablenote.cpp
@@ -3,57 +3,49 @@
 #include <cmath>
 
 PlayableNote::PlayableNote(double l, Interpolated<Frequency> freq, Interpolated<double> amp, double done) 
-    : m_length(l), m_frequency(freq), m_amplitude(amp), done(done) {
+    : PlayableNoteBase(l, done), m_frequency(freq), m_amplitude(amp) {
     m_frequency.SetInterpolator(logarithmicInterpolator<Frequency>);
     amp.SetInterpolator(logarithmicInterpolator<double>);
 }
 
 bool PlayableNote::IsComplete() const {
-    //double tp = 1/m_frequency(done/m_length).getFreq();
-    return done > m_length; //- (tp-fmod(m_length, tp));
+    //double tp = 1/m_frequency(m_done/m_length).getFreq();
+    return m_done > m_length; //- (tp-fmod(m_length, tp));
 }
 
 void PlayableNote::advance(int srate, double fm) {
     phase = fmod(phase + getDelta(srate) * fm, 1);
-    done += 1/double(srate);
+    m_done += 1/double(srate);
 }
 
 double PlayableNote::getDelta(int srate) {
-    return m_frequency(done/m_length).getFreq()/srate;
+    return m_frequency(m_done/m_length).getFreq()/srate;
 }
 double PlayableNote::getAmplitude(double t) {
     return m_amplitude(t);
-}
-double PlayableNote::getAmplitude() {
-    return m_amplitude(done/m_length);
 }
 
 double PlayableNote::getFreq(double t) {
     return m_frequency(t).getFreq();
 }
-double PlayableNote::getFreq() {
-    return m_frequency(done/m_length).getFreq();
-}
 
-PlayableNote::PlayableNote(std::istream& stream, double bpm) : Note(), m_length(0), m_frequency(), m_amplitude(1), done(0){
-    bool starts_with_parenthesis = false;
-    if((stream >> skipws).peek() == '(') {stream.get(); starts_with_parenthesis = true; }
-    if(!isNote((stream >> skipws).peek()))
-        throw parse_error(stream, "Cannot parse note");
-    m_frequency.Clear();
-    stream >> m_frequency;
+PlayableNote::PlayableNote(std::istream& stream, double bpm) : PlayableNoteBase(), m_frequency(), m_amplitude(1) {
+    brace(stream, '(', ')', [&](std::istream& stream) {
+        if(!isNote((stream >> skipws).peek()))
+            throw parse_error(stream, "Cannot parse note");
+        m_frequency.Clear();
+        stream >> m_frequency;
 
-    if(!isdigit((stream >> skipws).peek()))
-        throw parse_error(stream, std::string("Unexpected ") + char(stream.peek()));
-    stream >> m_length;
-    m_length *= 60/bpm;
+        if(!isdigit((stream >> skipws).peek()))
+            throw parse_error(stream, std::string("Unexpected ") + char(stream.peek()));
+        stream >> m_length;
+        m_length *= 60/bpm;
 
-    if(isdigit((stream >> skipws).peek())) {
-        m_amplitude.Clear();
-        stream >> m_amplitude;
-    }
-
-    if(starts_with_parenthesis) {stream >> expect(')');}
+        if(isdigit((stream >> skipws).peek())) {
+            m_amplitude.Clear();
+            stream >> m_amplitude;
+        }
+    });
 }
 
 std::unique_ptr<PlayableNote> PlayableNote::Create(std::istream& str, Tune*, double bpm, bool, int) {
--- src/player/playablenote.hpp
+++ src/player/playablenote.hpp
@@ -6,15 +6,42 @@
 #include "../audiosource.hpp"
 #include "note.hpp"
 
+class PlayableNoteBase : public Note {
+public:
+    virtual double getAmplitude(double t) = 0;
+    virtual const Interpolated<double>& getAmplitudeOverTime() const = 0;
+    virtual void setAmplitudeAt(double t, double value) = 0;
+    virtual void setAmplitude(Interpolated<double>&) = 0;
+    double getAmplitude() { return getAmplitude(m_done/m_length); }
+
+    virtual double getFreq(double t) = 0;
+    virtual const Interpolated<Frequency>& getFreqOverTime() const = 0;
+    virtual void setFreqAt(double t, Frequency value) = 0;
+    virtual void setFreq(Interpolated<Frequency>&) = 0;
+    double getFreq() { return getFreq(m_done/m_length); }
+
+    double GetLen() const override { return m_length; }
+    void SetLen(double len) { m_length = len; }
+    void ClampLen(double min, double max) {
+        if(m_length < min) m_length = min;
+        if(m_length > max) m_length = max;
+    }
+    bool IsComplete() const override { return m_done >= m_length; }
+
+
+protected:
+    double m_done = 0;
+    double m_length = 0;
+    PlayableNoteBase() : Note() {}
+    PlayableNoteBase(double len, double done = 0) : Note(), m_length(len), m_done(done) {}
+    PlayableNoteBase(const PlayableNoteBase& n) : Note(), m_done(n.m_done), m_length(n.m_length) {}
+};
+
 /// <summary> 
 /// Data for the generators to play.
 /// Syntax: <frequency> <length> [amplitude]
 /// </summary>
-class PlayableNote : public Note{
-    
-    /// <summary> The length of the note measured in seconds </summary>
-    double m_length = 1;
-
+class PlayableNote : public PlayableNoteBase{
     /// <summary> The frequency of the note measured in Hz </summary>
     Interpolated<Frequency> m_frequency = Frequency(0.0);
 
@@ -22,7 +49,6 @@ class PlayableNote : public Note{
     Interpolated<double> m_amplitude = 1;
 
     /// <summary> The amount that is completed measured in seconds </summary>
-    double done = 0;
     double phase = 0;
 
 public:
@@ -36,7 +62,7 @@ public:
     double getDelta(int srate = 48000);
 
     /// <summary> Gets how much has been elapsed since the start of this note in seconds </summary>
-     double getTime() { return done; }
+     double getTime() { return m_done; }
 
     /// <summary> Gets the length of this note </summary>
      double GetLen() const override { return m_length; }
@@ -47,15 +73,17 @@ public:
     }
 
     /// <summary> Gets the amplitude value at timestamp t </summary>
-    double getAmplitude(double t);
+    double getAmplitude(double t) override;
+    const Interpolated<double>& getAmplitudeOverTime() const override { return m_amplitude; }
+    void setAmplitudeAt(double t, double value) override { m_amplitude.Set(t, value); }
+    void setAmplitude(Interpolated<double>& ampl) override { m_amplitude = ampl; }
 
-    /// <summary> Gets the current amplitude value </summary>
-    double getAmplitude();
 
     /// <summary> Gets the frequency value at timestamp t </summary>
-    double getFreq(double t);
-    /// <summary> Gets the current frequency value </summary>
-    double getFreq();
+    double getFreq(double t) override;
+    const Interpolated<Frequency>& getFreqOverTime() const override { return m_frequency; }
+    void setFreqAt(double t, Frequency value) override { m_frequency.Set(t, value); } 
+    void setFreq(Interpolated<Frequency>& freq) override { m_frequency = freq; } 
     
     /// <summary> Checks whether the note is completed </summary>
     /// <remarks> A little more complicated than just "done > len", as that would chop off the end of the note... </remarks>
@@ -65,7 +93,7 @@ public:
 
     void AddSample(Source* p, int srate) override {
         double t = getTime();
-        double ampl = getAmplitude();
+        double ampl = PlayableNoteBase::getAmplitude();
         double freq_mul = p->getFrequencyMultiplier(t);
         (*p)(phase, t, srate, ampl);
         advance(srate, freq_mul);
